96c6d40f695a193aa62e95657a4cc73907850c3b
ShotaEbikawa <shotaebikawa@gmail.com>
2018-11-29 21:09:27 -0800

    added container

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,17 +6,20 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1>HOME</h1>
-                <p>
-                   This link redirects to page 1 <Link to="/page1/mail">Mail</Link>
-                </p>
-                <p>
-                    <a href="https://reacttraining.com/react-router/web/guides/quick-start">Click me to find out more about routing</a>
-                </p>
+                <div className="form-control">                
+                    <h1>HOME</h1>
+                    <p>
+                        This link redirects to page 1 <Link to="/page1/mail">Mail</Link>
+                    </p>
+                    <p>
+                        <a href="https://reacttraining.com/react-router/web/guides/quick-start">Click me to find out more about routing</a>
+                    </p>
+                </div>
             </div>
         );
     }
 }
+// I guess this is where I put my axios dismount
 
 const mapStateToProps = (state, ownProps) => {
     return {
